a5c065c3cda7c9bd27a6a0308f65ffa744852121
Ernesto Marquez <capitandoo@gmail.com>
2022-11-28 09:30:13 -0300

    creacion de la app

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 let nombre = prompt ("Hola!! Como estas? Ingresas tu nombre?");
 let reserva = prompt ("¿Habitación simple o doble?");
-let personas = prompt ("Miedo por preguntar...¿Cuantas personas?");
+let hidro = prompt ("¿Con hidromasaje? - si/no");
 let precio;
+let precioHidro;
+let totalreserva;
 
-while ((reserva == "") && (personas == "")) {
-    alert(nombre + " " + "habitación y cantidad de personas no pueden estar vacios");
+while ((reserva == "") && (hidro == "")) {
+    alert(nombre + " " + "Los datos no pueden estar vacios");
     nombre = prompt ("Hola!! Como estas? Ingresas tu nombre?");
     reserva = prompt ("¿Habitación simple o doble?");
-    personas = prompt ("Miedo por preguntar...¿Cuantas personas?");
+    hidro = prompt ("¿Con hidromasaje? - si/no");
 }
 
 if (reserva == "simple" || reserva == "doble"){
@@ -22,9 +24,19 @@ if (reserva == "simple" || reserva == "doble"){
                 break;
     }
 }else{
-    alert("impagable");
+    alert("sin opción");
 }
-while (reserva == "simple" || reserva == "doble"){
-    alert("total" + precio);
-    break;
+
+if (hidro == "si"){
+    precioHidro = 100;
+}else{
+    precioHidro = 0;
+    alert("sencillita entonces");
 }
+
+function total (){
+    totalreserva = precio + precioHidro;
+    alert(nombre + " el total de la reserva es de " + totalreserva);
+}
+
+total ();
